eec83da4893792664ccc1bbdc9f640e1a4fc631b
iammchemie <iammtfckin.alien@gmail.com>
2024-09-11 10:55:51 +0700

    login page, route done

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'username' => 'Username atau password salah.',
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GuestBookController;
 use App\Http\Controllers\AuthController;
 
+// Route untuk login
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-
 
 /*
 |--------------------------------------------------------------------------
@@ -18,17 +18,22 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 |
 */
 
+// Route untuk halaman utama
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Route untuk halaman login
 Route::get('/login', function () {
     return view('login');
 });
 
+// Route untuk halaman dashboard, dilindungi oleh middleware auth
+Route::get('/dashboard', function () {
+    return 'Selamat datang di Dashboard!';
+})->middleware('auth');
 
-<<<<<<< HEAD
-=======
+// Route untuk halaman GuestBook
 Route::get('/guestbook', [GuestBookController::class, 'index'])->name('guestbook.index');
 Route::get('/guestbook/create', [GuestBookController::class, 'create'])->name('guestbook.create');
 Route::post('/guestbook', [GuestBookController::class, 'store'])->name('guestbook.store');
